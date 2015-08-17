fe9d39e951d4a02d48e4a99d634d9c752676ee40
Soudamini <soudamini_patil@yahoo.com>
2015-08-17 11:07:12 +0530

    Update calc.cpp
    main() added

--- calc.cpp
+++ calc.cpp
@@ -1,2 +1,9 @@
 // This file is to create a sample code for calculator
-// File is created using gitHub
+
+// This is a new version of my file 
+# include <iostream.h>
+int main()
+{
+  cout<<"This is a Calculator Application";
+  
+}
